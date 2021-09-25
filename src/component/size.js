3198e8bb0ac61c953aebcdd21bef672e171830fa
程连杰 <2415908882@qq.com>
2021-09-25 14:40:49 +0800

    更换排版

--- src/component/size.js
+++ src/component/size.js
@@ -1,9 +1,19 @@
-import { Checkbox, Select, Button, Badge, InputNumber, Tooltip } from "antd";
+import { useState } from "react";
+import {
+  Checkbox,
+  Select,
+  Button,
+  Badge,
+  InputNumber,
+  Tooltip,
+  Drawer,
+} from "antd";
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import "./style/index.css";
 const plainOptions = ["XS", "S", "M", "ML", "L", "XL", "XXL"];
 
 const Size = () => {
+  const [visible, setVisible] = useState(false);
   const { Option } = Select;
   const checkboxOnchange = (checkedValues) => {
     console.log("checked = ", checkedValues);
@@ -13,6 +23,14 @@ const Size = () => {
   };
   const numberOnChange = (value) => {
     console.log(`number ${value}`);
+  };
+  // 关闭购物车
+  const onClose = () => {
+    setVisible(false);
+  };
+  // 打开购物车
+  const openShopingCar = () => {
+    setVisible(true);
   };
   return (
     <div className="box">
@@ -69,175 +87,184 @@ const Size = () => {
       {/* 购物车 */}
       <div className="right">
         <div>
-          <Badge count={5} size="small" offset={[-161, 15]}>
-            <div className="carts">
+          <Badge count={5} size="small" offset={[-301, 15]}>
+            <div className="carts" onClick={openShopingCar}>
               <ShoppingCartOutlined />
               购物车
             </div>
           </Badge>
-          <div className="shop-cart-item-box test-5">
-            <div className="shop-cart-item">
-              <div>
-                <img
-                  className="cart-img"
-                  src={require("./img/100_1.jpg")}
-                  alt=""
-                />
-              </div>
-              <div>
-                <div className="goods-tite">
-                  <Tooltip title="标题">Cat Tee Black T-Shirt</Tooltip>
+          <Drawer
+            width={530}
+            mask={false}
+            title="购物车"
+            placement="right"
+            onClose={onClose}
+            visible={visible}
+          >
+            <div className="shop-cart-item-box test-5">
+              <div className="shop-cart-item">
+                <div>
+                  <img
+                    className="cart-img"
+                    src={require("./img/100_1.jpg")}
+                    alt=""
+                  />
                 </div>
-                <div className="goods-sizes">型号：S,XS</div>
-                <div className="goods-privce">
-                  <span className="privace">价格：10</span>
-                  库存：10
+                <div>
+                  <div className="goods-tite">
+                    <Tooltip title="标题">Cat Tee Black T-Shirt</Tooltip>
+                  </div>
+                  <div className="goods-sizes">型号：S,XS</div>
+                  <div className="goods-privce">
+                    <span className="privace">价格：10</span>
+                    库存：10
+                  </div>
+                  数量:
+                  <InputNumber
+                    style={{ width: 80, height: 30 }}
+                    size="small"
+                    min={1}
+                    max={10}
+                    defaultValue={3}
+                    onChange={numberOnChange}
+                  />
                 </div>
-                数量:
-                <InputNumber
-                  style={{ width: 80, height: 30 }}
-                  size="small"
-                  min={1}
-                  max={10}
-                  defaultValue={3}
-                  onChange={numberOnChange}
-                />
+                <div className="del-button">
+                  <Button type="primary">删除</Button>
+                </div>
               </div>
-              <div className="del-button">
-                <Button type="primary">删除</Button>
+              <div className="shop-cart-item">
+                <div>
+                  <img
+                    className="cart-img"
+                    src={require("./img/100_1.jpg")}
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <div className="goods-tite">
+                    <Tooltip title="标题">Cat Tee Black T-Shirt</Tooltip>
+                  </div>
+                  <div className="goods-sizes">型号：S,XS</div>
+                  <div className="goods-privce">
+                    <span className="privace">价格：10</span>
+                    库存：10
+                  </div>
+                  数量:
+                  <InputNumber
+                    style={{ width: 80, height: 30 }}
+                    size="small"
+                    min={1}
+                    max={10}
+                    defaultValue={3}
+                    onChange={numberOnChange}
+                  />
+                </div>
+                <div className="del-button">
+                  <Button type="primary">删除</Button>
+                </div>
+              </div>
+              <div className="shop-cart-item">
+                <div>
+                  <img
+                    className="cart-img"
+                    src={require("./img/100_1.jpg")}
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <div className="goods-tite">
+                    <Tooltip title="标题">Cat Tee Black T-Shirt</Tooltip>
+                  </div>
+                  <div className="goods-sizes">型号：S,XS</div>
+                  <div className="goods-privce">
+                    <span className="privace">价格：10</span>
+                    库存：10
+                  </div>
+                  数量:
+                  <InputNumber
+                    style={{ width: 80, height: 30 }}
+                    size="small"
+                    min={1}
+                    max={10}
+                    defaultValue={3}
+                    onChange={numberOnChange}
+                  />
+                </div>
+                <div className="del-button">
+                  <Button type="primary">删除</Button>
+                </div>
+              </div>
+              <div className="shop-cart-item">
+                <div>
+                  <img
+                    className="cart-img"
+                    src={require("./img/100_1.jpg")}
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <div className="goods-tite">
+                    <Tooltip title="标题">Cat Tee Black T-Shirt</Tooltip>
+                  </div>
+                  <div className="goods-sizes">型号：S,XS</div>
+                  <div className="goods-privce">
+                    <span className="privace">价格：10</span>
+                    库存：10
+                  </div>
+                  数量:
+                  <InputNumber
+                    style={{ width: 80, height: 30 }}
+                    size="small"
+                    min={1}
+                    max={10}
+                    defaultValue={3}
+                    onChange={numberOnChange}
+                  />
+                </div>
+                <div className="del-button">
+                  <Button type="primary">删除</Button>
+                </div>
+              </div>
+              <div className="shop-cart-item">
+                <div>
+                  <img
+                    className="cart-img"
+                    src={require("./img/100_1.jpg")}
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <div className="goods-tite">
+                    <Tooltip title="标题">Cat Tee Black T-Shirt</Tooltip>
+                  </div>
+                  <div className="goods-sizes">型号：S,XS</div>
+                  <div className="goods-privce">
+                    <span className="privace">价格：10</span>
+                    库存：10
+                  </div>
+                  数量:
+                  <InputNumber
+                    style={{ width: 80, height: 30 }}
+                    size="small"
+                    min={1}
+                    max={10}
+                    defaultValue={3}
+                    onChange={numberOnChange}
+                  />
+                </div>
+                <div className="del-button">
+                  <Button type="primary">删除</Button>
+                </div>
               </div>
             </div>
-            <div className="shop-cart-item">
+            <div className="total">
+              <div> 合计：100</div>
               <div>
-                <img
-                  className="cart-img"
-                  src={require("./img/100_1.jpg")}
-                  alt=""
-                />
-              </div>
-              <div>
-                <div className="goods-tite">
-                  <Tooltip title="标题">Cat Tee Black T-Shirt</Tooltip>
-                </div>
-                <div className="goods-sizes">型号：S,XS</div>
-                <div className="goods-privce">
-                  <span className="privace">价格：10</span>
-                  库存：10
-                </div>
-                数量:
-                <InputNumber
-                  style={{ width: 80, height: 30 }}
-                  size="small"
-                  min={1}
-                  max={10}
-                  defaultValue={3}
-                  onChange={numberOnChange}
-                />
-              </div>
-              <div className="del-button">
-                <Button type="primary">删除</Button>
+                <Button type="primary">结算</Button>
               </div>
             </div>
-            <div className="shop-cart-item">
-              <div>
-                <img
-                  className="cart-img"
-                  src={require("./img/100_1.jpg")}
-                  alt=""
-                />
-              </div>
-              <div>
-                <div className="goods-tite">
-                  <Tooltip title="标题">Cat Tee Black T-Shirt</Tooltip>
-                </div>
-                <div className="goods-sizes">型号：S,XS</div>
-                <div className="goods-privce">
-                  <span className="privace">价格：10</span>
-                  库存：10
-                </div>
-                数量:
-                <InputNumber
-                  style={{ width: 80, height: 30 }}
-                  size="small"
-                  min={1}
-                  max={10}
-                  defaultValue={3}
-                  onChange={numberOnChange}
-                />
-              </div>
-              <div className="del-button">
-                <Button type="primary">删除</Button>
-              </div>
-            </div>
-            <div className="shop-cart-item">
-              <div>
-                <img
-                  className="cart-img"
-                  src={require("./img/100_1.jpg")}
-                  alt=""
-                />
-              </div>
-              <div>
-                <div className="goods-tite">
-                  <Tooltip title="标题">Cat Tee Black T-Shirt</Tooltip>
-                </div>
-                <div className="goods-sizes">型号：S,XS</div>
-                <div className="goods-privce">
-                  <span className="privace">价格：10</span>
-                  库存：10
-                </div>
-                数量:
-                <InputNumber
-                  style={{ width: 80, height: 30 }}
-                  size="small"
-                  min={1}
-                  max={10}
-                  defaultValue={3}
-                  onChange={numberOnChange}
-                />
-              </div>
-              <div className="del-button">
-                <Button type="primary">删除</Button>
-              </div>
-            </div>
-            <div className="shop-cart-item">
-              <div>
-                <img
-                  className="cart-img"
-                  src={require("./img/100_1.jpg")}
-                  alt=""
-                />
-              </div>
-              <div>
-                <div className="goods-tite">
-                  <Tooltip title="标题">Cat Tee Black T-Shirt</Tooltip>
-                </div>
-                <div className="goods-sizes">型号：S,XS</div>
-                <div className="goods-privce">
-                  <span className="privace">价格：10</span>
-                  库存：10
-                </div>
-                数量:
-                <InputNumber
-                  style={{ width: 80, height: 30 }}
-                  size="small"
-                  min={1}
-                  max={10}
-                  defaultValue={3}
-                  onChange={numberOnChange}
-                />
-              </div>
-              <div className="del-button">
-                <Button type="primary">删除</Button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="total">
-          <div> 合计：100</div>
-          <div>
-            <Button type="primary">结算</Button>
-          </div>
+          </Drawer>
         </div>
       </div>
     </div>
