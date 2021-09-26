2a0328203db568c5e1bb8f7f70852f43fdd7b3cb
程连杰 <chenglianjie@fanxiapp.com>
2021-09-26 11:02:14 +0800

    错误处理

--- src/request/request.js
+++ src/request/request.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 // 获取商品列表数据
 const getProductData = async (obj) => {
-  const {
-    data: { data },
-  } = await axios.get("http://120.55.193.14:3030/shoppingCat", { params: obj });
-  return data;
+  try {
+    const {
+      data: { data },
+    } = await axios.get("http://120.55.193.14:3030/shoppingCat", { params: obj });
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 export { getProductData };
